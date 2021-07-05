2a91037e8c397d333cd6ddc1e7403b9d184a5329
OfficialLawyerEmporium <OfficialLawyerEmporium@gmail.com>
2021-07-05 00:56:52 -0700

    #4

--- start_server.py
+++ start_server.py
@@ -190,7 +190,7 @@ def music2yaml(yaml_path, path):
 
             process = subprocess.run(
                 ["ffprobe", "-v", "error", "-show_entries", "format=duration",
-                "-of", "default=noprint_wrappers=1:nokey=1", file_path],
+                "-of", "default=noprint_wrappers=1:nokey=1"], input=file_path,
                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT
             )
             out = process.stdout
