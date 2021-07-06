4f74c0cac04a6e706869ac6a815a231b72851717
OfficialLawyerEmporium <OfficialLawyerEmporium@gmail.com>
2021-07-05 21:57:08 -0700

    Update start_server.py

--- start_server.py
+++ start_server.py
@@ -246,7 +246,7 @@ def music2yaml(yaml_path, path):
                 filename = track["name"].replace("] ", "]   ").split("  ")[0]
 
                 for obj in tags_categories_songs:
-                    if filename == obj.get("tag")
+                    if filename == obj.get("tag"):
                         print("----------")
                         print("Tag match!")
                         print(obj.get("tag"))
